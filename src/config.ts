d145c4b93b537302cc371ff5ce161abe7a0b71b1
ShiroRikka <2985772684@qq.com>
2025-03-09 03:46:48 +0800

    rm search by trae

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,7 @@ const config: Config = {
       //   ]
       // },
     ],
-    hasSearchToggle: true,
+    hasSearchToggle: false,
     hasThemeToggle: true,
   },
   hero: {
--- src/partials/Sidebar/MobileDrawer.tsx
+++ src/partials/Sidebar/MobileDrawer.tsx
@@ -3,12 +3,12 @@ import { useAtom, useAtomValue } from 'jotai';
 import { 
   sidebarDrawerVisible as sidebarDrawerVisibleAtom,
   hasThemeToggle as hasThemeToggleAtom,
-  hasSearchToggle as hasSearchToggleAtom,
+
 } from "@/store/atoms";
 import Drawer from '@/components/Drawer';
 import { twMerge } from 'tailwind-merge';
 import ThemeToggle from '@/components/ThemeToggle';
-import SearchToggle from '@/components/SearchToggle';
+
 
 export interface MobileDrawerProps extends React.PropsWithChildren<React.ComponentPropsWithoutRef<'aside'>> {}
 
@@ -20,13 +20,13 @@ export default function MobileDrawer({
 
   const [drawerVisible, setDrawerVisible] = useAtom(sidebarDrawerVisibleAtom);
   const hasThemeToggle = useAtomValue(hasThemeToggleAtom);
-  const hasSearchToggle = useAtomValue(hasSearchToggleAtom);
+
 
   return (
     <Drawer open={drawerVisible} onOpenChange={setDrawerVisible}>
       <Drawer.Content>
         <div className="px-3 h-14 flex gap-2 items-center">
-          {hasSearchToggle && <SearchToggle onOpenChange={(open) => { open && setDrawerVisible(false) }} />}
+
           {hasThemeToggle && <ThemeToggle />}
           <Drawer.Close className='ml-auto' />
         </div>
